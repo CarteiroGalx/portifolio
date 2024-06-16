3e4b09e577bff37744bb6275f9a59a1948d89ace
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-16 19:57:20 -0300

    Finalização da barra de navegação concretizada

--- portifolio-2.0/src/components/nav.js
+++ portifolio-2.0/src/components/nav.js
@@ -2,10 +2,12 @@ import { Header } from './nav.jsx'
 
 function nav(){
     return(
-        <Header className="row pt-4 pb-5">
-            <a className="text-white col-8 text-end text-decoration-none fs-5 p-0" href="#"><i className="bi bi-house"></i>Home</a>
-            <a className="text-white col-2 text-decoration-none fs-5 p-0" href="#"><i className="bi bi-tools p-0"></i>Projetos</a>
-            <a className="text-white col-2 text-decoration-none fs-5 p-0 text-start" href="#"><i className="bi bi-telephone"></i>Contato</a>     
+        <Header className="row pb-5 d-flex align-items-center">
+            <img className='col-1 mx-4 mt-2' src="/images/logo.png" alt="Logo do criador" />
+            <div className='col-5'></div>
+            <a className="col-1 text-end text-decoration-none fs-5 p-0" href="#"><i className="bi bi-house"></i>Home</a>
+            <a className="col-1 mx-5 text-decoration-none fs-5 p-0" href="#"><i className="bi bi-tools p-0"></i>Projetos</a>
+            <a className="col-1 text-decoration-none fs-5 p-0 text-start" href="#"><i className="bi bi-telephone"></i>Contato</a>     
         </Header>
     )
 }
--- portifolio-2.0/src/components/nav.jsx
+++ portifolio-2.0/src/components/nav.jsx
@@ -3,4 +3,18 @@ import styled from 'styled-components'
 export const Header = styled.div`
   background: rgb(0, 0, 0);
   background: linear-gradient(180deg, #000000 67%, rgba(0,0,0,0) 100%);
+
+  img{
+    
+  }
+
+  a {
+    color: white; 
+    text-decoration: none; 
+
+    &:hover {
+      cursor: pointer;
+      color: #007BFF;
+    }
+  }
 `
