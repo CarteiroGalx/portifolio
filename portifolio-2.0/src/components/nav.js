eecacfa07319ff2b5bf00ce5b9bb308661d0a90f
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-16 01:28:45 -0300

    Resolvendo desatualizações e finalizando cabeçalho

--- portifolio-2.0/src/components/nav.js
+++ portifolio-2.0/src/components/nav.js
@@ -1,14 +1,12 @@
-import styles from './nav.module.css';
+import { Header } from './nav.jsx'
 
 function nav(){
     return(
-        <div>
-            <div className={`row bg-dark pt-4 pb-5 ${styles.background}`}>
-                <a className="text-white col-8 text-end text-decoration-none fs-5 p-0" href="#"><i className="bi bi-house"></i>Home</a>
-                <a className="text-white col-2 text-decoration-none fs-5 p-0" href="#"><i className="bi bi-tools p-0"></i>Projetos</a>
-                <a className="text-white col-2 text-decoration-none fs-5 p-0 text-start" href="#"><i className="bi bi-telephone"></i>Contato</a>       
-            </div>
-        </div>
+        <Header className="row pt-4 pb-5">
+            <a className="text-white col-8 text-end text-decoration-none fs-5 p-0" href="#"><i className="bi bi-house"></i>Home</a>
+            <a className="text-white col-2 text-decoration-none fs-5 p-0" href="#"><i className="bi bi-tools p-0"></i>Projetos</a>
+            <a className="text-white col-2 text-decoration-none fs-5 p-0 text-start" href="#"><i className="bi bi-telephone"></i>Contato</a>     
+        </Header>
     )
 }
 
---
+++ portifolio-2.0/src/components/nav.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+  background: rgb(0, 0, 0);
+  background: linear-gradient(180deg, #000000 67%, rgba(0,0,0,0) 100%);
+`
