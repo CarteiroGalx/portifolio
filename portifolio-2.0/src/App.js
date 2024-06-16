98055eeddddceb12f667dba78cdfe359b006419e
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-16 20:19:40 -0300

    Inicializando aba de apresentação já com alguns elementos

--- portifolio-2.0/src/App.js
+++ portifolio-2.0/src/App.js
@@ -5,9 +5,13 @@ import Nav from './components/nav'
 function App() {
   return (
     <div className="App">
-      <header>
         <Nav />
-      </header>
+        <div className='row'>
+          <div className='col-8 apresentation'>
+            <h1>Olá, meu nome é <span>Erivelton</span></h1>
+            <h3>Desenvolvedor Front-End & Game Developer. Um apaixonado pela tecnologia que mora em PE, Recife.</h3>
+          </div>
+        </div>
     </div>
   );
 }
--- portifolio-2.0/src/components/nav.jsx
+++ portifolio-2.0/src/components/nav.jsx
@@ -4,9 +4,8 @@ export const Header = styled.div`
   background: rgb(0, 0, 0);
   background: linear-gradient(180deg, #000000 67%, rgba(0,0,0,0) 100%);
 
-  img{
-    
-  }
+  position: fixed;
+  width: 100%;
 
   a {
     color: white; 
