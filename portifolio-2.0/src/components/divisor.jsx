6cf99e0a554250669a447b3b04caf09707334edf
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-17 03:18:30 -0300

    Novo divisor entre aba de apresentação e aba de projetos

---
+++ portifolio-2.0/src/components/divisor.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Divisorline = styled.div`
+
+h3{
+    max-width: 350px;
+    color: #062C65;
+    font-size: 2.8rem;
+    text-align: center;
+}
+
+.divisor{
+  width: 70%;
+  height: 8px;
+  border-radius: 20px;
+  background-color: #062C65;
+}
+`
--- portifolio-2.0/src/components/nav.jsx
+++ portifolio-2.0/src/components/nav.jsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Header = styled.div`
+ z-index: 2;
   background: rgb(0, 0, 0);
   background: linear-gradient(180deg, #000000 67%, rgba(0,0,0,0) 100%);
 
