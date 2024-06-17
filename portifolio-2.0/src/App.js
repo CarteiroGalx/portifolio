af99d5d9fc2294470fdfe57cf371fdc226709528
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-16 21:53:04 -0300

    Adicionando animação GIF e mudando sua opacidade

--- portifolio-2.0/src/App.js
+++ portifolio-2.0/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
         <Nav />
         <div className='row'>
-          <div className='col-6 apresentation'>
+          <div className='col-6 apresentation mx-5'>
             <div className='col-10'>
               <h1>Olá, meu nome é <span>Erivelton</span></h1>
               <h5>Desenvolvedor Front-End & Game Developer. Um apaixonado pela tecnologia que mora em PE, Recife.</h5>
@@ -16,10 +16,13 @@ function App() {
                 <i class="bi bi-github mx-1 icons-apresentation"></i>
               </div>
               <div className='col-12 row mt-4 align-itens-center d-flex'>
-                <h3 className='col-4 px-0 b-blue-font'>Conhecimentos</h3>
-                <div className='col mx-2 divisor-knowledge'></div>
+                <h3 className='col-4 px-0 b-blue-font mx-3'>Conhecimentos</h3>
+                <div className='col divisor-knowledge'></div>
               </div>
             </div>
+          </div>
+          <div className='col apresentation'>
+            <img className='animation-logo' src='/images/animation-logo.gif'></img>
           </div>
         </div>
     </div>
