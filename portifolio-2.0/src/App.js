a25daf1d220a97d2507228fc7be5c3970ad1bf3a
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-16 22:53:28 -0300

    Adicionando e finalizando template da aba de apresentação

--- portifolio-2.0/src/App.js
+++ portifolio-2.0/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
         <Nav />
         <div className='row'>
-          <div className='col-6 apresentation mx-5'>
+          <div className='col-7 apresentation mx-5'>
             <div className='col-10'>
               <h1>Olá, meu nome é <span>Erivelton</span></h1>
               <h5>Desenvolvedor Front-End & Game Developer. Um apaixonado pela tecnologia que mora em PE, Recife.</h5>
@@ -15,13 +15,27 @@ function App() {
                 <i class="bi bi-linkedin mx-1 icons-apresentation"></i>
                 <i class="bi bi-github mx-1 icons-apresentation"></i>
               </div>
-              <div className='col-12 row mt-4 align-itens-center d-flex'>
+              <div className='col-12 row mt-4 align-items-center d-flex'>
                 <h3 className='col-4 px-0 b-blue-font mx-3'>Conhecimentos</h3>
-                <div className='col divisor-knowledge'></div>
+                <div className='col divisor'></div>
+                <div className='icons-knowledge align-items-center justify-content-around d-flex'>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-1.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-2.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-3.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-4.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-5.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-6.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-7.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-8.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-9.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-10.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-11.png'></img>
+                  <img className='icon-knowledge mx-1' src='/images/icons-knowledge/icon-12.png'></img>
+                </div>
               </div>
             </div>
           </div>
-          <div className='col-4 apresentation'>
+          <div className='col apresentation'>
             <img className='animation-logo' src='/images/animation-logo.gif'></img>
           </div>
         </div>
