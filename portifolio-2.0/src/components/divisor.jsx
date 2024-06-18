04680c8a805a642c3b95556fde71b4f2b277f53f
Eriver18 <almeidalopeserivelton@gmail.com>
2024-06-18 18:12:27 -0300

    Divisor de projetos com responsividade

--- portifolio-2.0/src/components/divisor.jsx
+++ portifolio-2.0/src/components/divisor.jsx
@@ -4,15 +4,16 @@ export const Divisorline = styled.div`
 
 h3{
     max-width: 300px;
-    color: #062C65;
+    color: #062c6584;
     font-size: 2.8rem;
     text-align: center;
 }
 
 .divisor{
-  width: 70%;
-  height: 8px;
-  border-radius: 20px;
-  background-color: #062C65;
+    margin-right: 2vw;
+    width: 85vw;
+    height: 0.6vh;
+    border-radius: 20px;
+    background-color: #062c6584;
 }
 `
